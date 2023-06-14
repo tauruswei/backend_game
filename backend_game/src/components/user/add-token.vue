e5688ea8a98e2c4e6487e04d6723b6e3e587d667
liudongxue <1096074393@qq.com>
2023-06-14 11:45:37 +0800

    修改

---
+++ backend_game/src/components/user/add-token.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <el-button type="danger" @click="open()" round>Add token to wallet</el-button>
+    <el-dialog v-model="visible" title="Add to wallet manually" width="400px" destroy-on-close append-to-body>
+      <el-row :gutter="5" style="margin-bottom:20px">
+        <el-col :span="4">
+          <span style="color:red">*</span>Contract Address
+        </el-col>
+        <el-col :span="20">
+          <el-input v-model="address" style="width:100%" clearable required></el-input>
+        </el-col>
+      </el-row>
+      <el-row :gutter="5">
+        <el-col :span="4" style="margin-bottom:20px"><span style="color:red">*</span>Symbol</el-col>
+        <el-col :span="20" style="margin-bottom:20px">
+          <el-input v-model="symbol" style="width:100%" clearable required></el-input>
+        </el-col>
+        <el-col :span="4">Image url</el-col>
+        <el-col :span="20">
+          <el-input v-model="image" style="width:100%" clearable required></el-input>
+        </el-col>
+        <el-col :span="24" style="margin-top:15px">
+          <el-button type="primary" @click="addToken()" style="width:100%">
+            Sure
+          </el-button>
+        </el-col>
+      </el-row>
+    </el-dialog>
+  </div>
+</template>
+<script setup>
+import { ref,getCurrentInstance } from "vue";
+import {useStore} from "vuex"
+import { loadingHelper } from "@/utils/loading";
+import { ASSETTYPE, TXTYPE, savaAfterTranscation } from "@/utils/meta-mask";
+import {base64} from "@/utils/base64"
+const store = useStore()
+let CONTRACTS = store.state.abi.contract;
+const emit = defineEmits(['balance'])
+const {proxy} = getCurrentInstance();
+const metaMask = proxy.metaMask;
+const address = ref("")
+const symbol = ref("")
+const image = ref("")
+const visible = ref(false)
+
+async function open() {
+  if (!metaMask.isAvailable()) return;
+  visible.value = true
+}
+async function addToken() {
+  if (!metaMask.isAvailable()) return;
+  if(!address.value) {
+    ElMessage.error("Contract address is required")
+    return
+  }
+  if(!symbol.value) {
+    ElMessage.error("Symbol is required")
+    return
+  }
+  let param ={
+        address: address.value,
+        symbol: symbol.value,
+        image: image.value||null
+    }
+    loadingHelper.show()
+    await metaMask.watchAsset(param);
+    loadingHelper.hide()
+    visible.value = false
+    await emit("balance");
+}
+</script>
---
+++ backend_game/src/components/user/trans-table.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <el-table :data="listData" style="width:100%;min-height:360px">
+      <el-table-column label="No." min-width="5%">
+        <template #default="scope">
+            {{ (scope.$index + 1) * pageNum }}
+        </template>
+      </el-table-column>
+      <el-table-column label="From" prop="fromAmount" min-width="10%"></el-table-column>
+      <el-table-column label="To" prop="toAmount" min-width="10%"></el-table-column>
+      <el-table-column label="txID" prop="txId" min-width="30%"></el-table-column>
+      <el-table-column label="Create time" min-width="25%" prop="createTime"></el-table-column>
+      <el-table-column label="Update time" min-width="20%" prop="updateTime"></el-table-column>
+      <template #empty>
+        <el-empty style="margin:0 auto"></el-empty>
+      </template>
+    </el-table>
+    <el-pagination background layout="prev, pager, next" :total="total" :current-page="pageNum" @current-change="handlePageChange" :page-size="pageSize" />
+  </div>
+</template>
+<script setup>
+import { ref, watch } from "vue";
+import { chainApi } from "@/api/request";
+import { DateHelper } from "@/utils/helper";
+import {useStore} from "vuex";
+const store = useStore()
+const pageSize = ref(10);
+const pageNum = ref(1)
+const total = ref(1)
+const listData = ref([]);
+const props = defineProps({
+  txtype: { type: Number },
+})
+watch(() => props.txtype, (val) => {
+  if (val != undefined) {
+    pageNum.value = 1;
+    query(val)
+  }  
+}, { immediate: true })
+function handlePageChange(val) {
+  pageNum.value = val;
+  query()
+}
+function query(txtype) {
+  let data = {
+    pageSize: pageSize.value,
+    pageNo: pageNum.value,
+    userId: store.state.user.id,
+    transType: txtype,
+    status: 1
+  }
+  chainApi.list(data).then((res) => {
+    if (res.code == 0) {
+      listData.value = res.data.list;
+      listData.value.forEach(item=>{
+        if(item.createTime) item.createTime = DateHelper.toString(item.createTime)
+        if(item.updateTime) item.updateTime = DateHelper.toString(item.updateTime)
+      }) 
+      total.value = res.data.total;
+    }
+  })
+}
+</script>
